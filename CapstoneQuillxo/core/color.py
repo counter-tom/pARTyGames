c438b923a1647960d613ccdecf9b94278f1a7909
Tom <tshaw1191@outlook.com>
2026-04-10 16:57:04 -0400

    Color fix

--- CapstoneQuillxo/core/color.py
+++ CapstoneQuillxo/core/color.py
@@ -10,9 +10,7 @@ class Color(Enum):
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
     BLUE = (0, 0, 255)
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-
+    
 
 class Tool(Enum):
     BRUSH = 1
