63bd2c377024accaa7daca1961f47984fe87f157
Tom <tshaw1191@outlook.com>
2026-04-10 16:53:31 -0400

    Added colors to core/color.py. Fixed file structure

--- CapstoneQuillxo/core/color.py
+++ CapstoneQuillxo/core/color.py
@@ -6,6 +6,13 @@ class Color(Enum):
     GREY = (128, 128, 128)
     BUTTON_INACTIVE_GREY = (212, 212, 212)
     BUTTON_ACTIVE_GREY = (73, 72, 72)
+    PURPLE = (128, 0, 128)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+
 
 class Tool(Enum):
     BRUSH = 1
